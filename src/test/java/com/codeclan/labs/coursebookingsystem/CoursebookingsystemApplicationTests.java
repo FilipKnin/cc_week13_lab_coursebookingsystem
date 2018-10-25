0645691708113f5f6f60df7aeba829e4c81d03a5
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-25 11:36:45 +0100

    canAddCourse tested

--- src/test/java/com/codeclan/labs/coursebookingsystem/CoursebookingsystemApplicationTests.java
+++ src/test/java/com/codeclan/labs/coursebookingsystem/CoursebookingsystemApplicationTests.java
@@ -1,7 +1,12 @@
 package com.codeclan.labs.coursebookingsystem;
 
+import com.codeclan.labs.coursebookingsystem.models.Booking;
+import com.codeclan.labs.coursebookingsystem.models.Course;
+import com.codeclan.labs.coursebookingsystem.repositories.BookingRepository;
+import com.codeclan.labs.coursebookingsystem.repositories.CourseRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +14,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CoursebookingsystemApplicationTests {
 
+	@Autowired
+	CourseRepository courseRepository;
+
+	@Autowired
+	BookingRepository bookingRepository;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void canAddCourse() {
+		Course course = new Course("Course1", "Edinburgh", 5);
+		courseRepository.save(course);
 	}
 
 }
