18f4e002182d078d5617bb791f776ebacd6e2124
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-25 11:32:14 +0100

    CourseController created

--- src/main/java/com/codeclan/labs/coursebookingsystem/controllers/CourseController.java
+++ src/main/java/com/codeclan/labs/coursebookingsystem/controllers/CourseController.java
@@ -1,4 +1,16 @@
 package com.codeclan.labs.coursebookingsystem.controllers;
 
+import com.codeclan.labs.coursebookingsystem.repositories.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/courses")
+
 public class CourseController {
+
+    @Autowired
+    CourseRepository courseRepository;
+
 }
